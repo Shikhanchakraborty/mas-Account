d4f8479b0ef7001fa2befc54d621b23be3c51756
asikurrahmanshikhan <asikur.rahman987@gmail.com>
2021-11-23 14:06:49 +0600

    mas account

---
+++ java.js
@@ -0,0 +1,118 @@
+const loginBtn = document.getElementById("button1");
+loginBtn.addEventListener("click",function(){
+    const loginArea = document.getElementById("login")
+    loginArea.style.display = "none"
+    const secondArea =document.getElementById("container1")
+    secondArea.style.display = "block"
+})
+
+const loginBtn2 = document.getElementById("little");
+loginBtn2.addEventListener('click',function(){
+    const loginArea2 =document.getElementById('container1')
+    loginArea2.style.display = "none"
+    const secondArea2 =document.getElementById("container2")
+    secondArea2.style.display = "block"
+
+})
+
+const loginBtn3 = document.getElementById('little1');
+loginBtn3.addEventListener('click', function (){
+    const loginArea3 = document.getElementById('container1')
+    loginArea3.style.display = 'none'
+    const secondArea3 = document.getElementById('container3')
+    secondArea3.style.display= 'block'
+})     
+
+
+
+
+const loginBtn4 = document.getElementById('little2');
+loginBtn4.addEventListener('click', function (){
+    const loginArea4 = document.getElementById('container1')
+    loginArea4.style.display = 'none'
+    const secondArea4 = document.getElementById('container4')
+    secondArea4.style.display= 'block'
+})  
+
+
+
+const loginBtn5 = document.getElementById('little3');
+loginBtn5.addEventListener('click', function (){
+    const loginArea5 = document.getElementById('container1')
+    loginArea5.style.display = 'none'
+    const secondArea5 = document.getElementById('container5')
+    secondArea5.style.display= 'block'
+})  
+
+
+const loginBtn6 = document.getElementById('little4');
+loginBtn6.addEventListener('click', function (){
+    const loginArea6 = document.getElementById('container1')
+    loginArea6.style.display = 'none'
+    const secondArea6 = document.getElementById('container6')
+    secondArea6.style.display= 'block'
+})  
+
+
+
+const loginBtn7 = document.getElementById('little5');
+loginBtn7.addEventListener('click', function (){
+    const loginArea7 = document.getElementById('container1')
+    loginArea7.style.display = 'none'
+    const secondArea7 = document.getElementById('container6')
+    secondArea7.style.display= 'block'
+})  
+
+
+
+const loginBtn8 = document.getElementById('little6');
+loginBtn8.addEventListener('click', function (){
+    const loginArea8 = document.getElementById('container1')
+    loginArea8.style.display = 'none'
+    const secondArea8 = document.getElementById('container6')
+    secondArea8.style.display= 'block'
+}) 
+
+
+const loginBtn9 = document.getElementById('little7');
+loginBtn9.addEventListener('click', function (){
+    const loginArea9 = document.getElementById('container1')
+    loginArea9.style.display = 'none'
+    const secondArea9 = document.getElementById('container6')
+    secondArea9.style.display= 'block'
+}) 
+
+const loginBtn10 = document.getElementById('little8');
+loginBtn10.addEventListener('click', function (){
+    const loginArea10 = document.getElementById('container1')
+    loginArea10.style.display = 'none'
+    const secondArea10 = document.getElementById('container6')
+    secondArea10.style.display= 'block'
+}) 
+
+
+const loginBtn11 = document.getElementById('little9');
+loginBtn11.addEventListener('click', function (){
+    const loginArea11 = document.getElementById('container1')
+    loginArea11.style.display = 'none'
+    const secondArea11 = document.getElementById('container6')
+    secondArea11.style.display= 'block'
+}) 
+
+
+const loginBtn12 = document.getElementById('little10');
+loginBtn12.addEventListener('click', function (){
+    const loginArea12 = document.getElementById('container1')
+    loginArea12.style.display = 'none'
+    const secondArea12 = document.getElementById('container6')
+    secondArea12.style.display= 'block'
+}) 
+
+
+const loginBtn13 = document.getElementById('little11');
+loginBtn13.addEventListener('click', function (){
+    const loginArea13 = document.getElementById('container1')
+    loginArea13.style.display = 'none'
+    const secondArea13 = document.getElementById('container6')
+    secondArea13.style.display= 'block'
+}) 
